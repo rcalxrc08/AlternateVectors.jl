6dde4af2b0205ad85344625a6fe1f1d26dd352b7
Nicola <nicola.scaramuzzino@yandex.com>
2023-11-04 18:48:10 +0100

    impl

--- src/AlternateVectors.jl
+++ src/AlternateVectors.jl
@@ -1,5 +1,7 @@
 module AlternateVectors
-
+include("alternate_vectors.jl")
+include("alternate_padded_vectors.jl")
+export AlternateVector, AlternatePaddedVector
 # Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,62 @@
 using AlternateVectors
 using Test
 
-@testset "AlternateVectors.jl" begin
-    # Write your tests here.
+@testset "AlternateVectors arithmetical operations closed" begin
+	N=11
+	av=AlternateVector(-2.0,3.0,N)
+	@test av[1]==-2.0
+	@test av[2]==3.0
+	@test av[end]==-2.0
+	println(av)
+	av_c=collect(av)
+	@test typeof(av[1:2])<:AlternateVector
+	@test typeof(1 .+ av)<:AlternateVector
+	@test typeof(av.+1)<:AlternateVector
+	@test typeof(sin.(av))<:AlternateVector
+	@test typeof(@. sin(av)*av+1+av)<:AlternateVector
+	@test typeof(@. sin(av)*av+1+exp(av))<:AlternateVector
+	@test typeof(@. sin(av)*av*av+1+exp(av))<:AlternateVector
+	@test typeof(@. 2+sin(av)*av+1+exp(av))<:AlternateVector
+	@test all(@. av ≈ av_c)
+	@test all(@. sin(av) ≈ sin(av_c))
+	@test all(@. exp(av)+av ≈ exp(av_c)+av_c)
+	@test all(@. exp(av)+av_c ≈ exp(av_c)+av)
+	@test all(@. exp(av)+av_c ≈ exp(av_c)+av_c)
+	@test all(@. exp(av)+av_c+av*av_c ≈ exp(av_c)+av_c+av*av_c)
+	@test all(@. exp(av)+av+av*av ≈ exp(av_c)+av_c+av*av_c)
+end
+
+
+
+@testset "arithmetical operations closed" begin
+	N=11
+	av=AlternatePaddedVector(-2.0,3.0,2.0,4.0,N)
+	@test_throws "length of AlternatePaddedVector must be greater than 2." AlternatePaddedVector(1,1,1,1,1)
+	@test_throws "Trying to getindex with an AbstractRange of length" av[1:1]
+	@test av[1]==-2.0
+	@test av[2]==3.0
+	@test av[3]==2.0
+	@test av[4]==3.0
+	@test av[end]==4.0
+	println(av)
+	av_c=collect(av)
+	@test typeof(av[1:2])<:AlternatePaddedVector
+	@test typeof(1 .+ av)<:AlternatePaddedVector
+	@test typeof(av.+1)<:AlternatePaddedVector
+	@test typeof(sin.(av))<:AlternatePaddedVector
+	@test typeof(@. sin(av)*av+1+av)<:AlternatePaddedVector
+	@test typeof(@. sin(av)*av+1+exp(av))<:AlternatePaddedVector
+	@test typeof(@. sin(av)*av*av+1+exp(av))<:AlternatePaddedVector
+	@test typeof(@. 2+sin(av)*av+1+exp(av))<:AlternatePaddedVector
+	@test all(@. av ≈ av_c)
+	@test all(@. av[1:5] ≈ av_c[1:5])
+	@test all(@. av[3:2:9] ≈ av_c[3:2:9])
+	@test all(@. av[8:9] ≈ av_c[8:9])
+	@test all(@. sin(av) ≈ sin(av_c))
+	@test all(@. exp(av)+av ≈ exp(av_c)+av_c)
+	@test all(@. exp(av)+av+2 ≈ exp(av_c)+av_c+2)
+	@test all(@. exp(av)+av_c ≈ exp(av_c)+av)
+	@test all(@. exp(av)+av_c ≈ exp(av_c)+av_c)
+	@test all(@. exp(av)+av_c+av*av_c ≈ exp(av_c)+av_c+av*av_c)
+	@test all(@. exp(av)+av+av*av ≈ exp(av_c)+av_c+av*av_c)
 end
