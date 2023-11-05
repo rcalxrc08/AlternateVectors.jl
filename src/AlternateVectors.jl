8f6615568e831ec18f6838cc11af5f782c95fead
Nicola <nicola.scaramuzzino@yandex.com>
2023-11-05 15:02:49 +0100

    remove cuda

--- src/AlternateVectors.jl
+++ src/AlternateVectors.jl
@@ -1,8 +1,4 @@
 module AlternateVectors
-using Requires # for conditional dependencies
-function __init__()
-    @require CUDA = "052768ef-5323-5732-b1bb-66c8b64840ba" include("deps/gpu_dependencies.jl")
-end
 include("alternate_vectors.jl")
 include("alternate_padded_vectors.jl")
 export AlternateVector, AlternatePaddedVector
--- src/deps/gpu_dependencies.jl
+++
@@ -1,9 +0,0 @@
-using .CUDA
-
-CUDA.allowscalar(false)
-
-# Base.BroadcastStyle(a::ArrayStyleAlternatePaddedVector, ::CUDA.CuArrayStyle{0}) = a
-# Base.BroadcastStyle(::ArrayStyleAlternatePaddedVector, a::CUDA.CuArrayStyle{N}) where {N} = a
-
-# Base.BroadcastStyle(a::ArrayStyleAlternateVector, ::CUDA.CuArrayStyle{0}) = a
-# Base.BroadcastStyle(::ArrayStyleAlternateVector, a::CUDA.CuArrayStyle{N}) where {N} = a
