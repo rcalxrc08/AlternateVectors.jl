52deca9da0efffed23e1cffce68ace5b0fa35b4a
Nicola <nicola.scaramuzzino@yandex.com>
2024-01-07 13:13:54 +0100

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,25 @@
 using AlternateVectors
 using Documenter
 
-DocMeta.setdocmeta!(AlternateVectors, :DocTestSetup, :(using AlternateVectors); recursive=true)
+DocMeta.setdocmeta!(
+    AlternateVectors,
+    :DocTestSetup,
+    :(using AlternateVectors);
+    recursive = true
+)
 
 makedocs(;
-    modules=[AlternateVectors],
-    authors="Nicola <nicola.scaramuzzino@yandex.com> and contributors",
-    repo="https://github.com/rcalxrc08/AlternateVectors.jl/blob/{commit}{path}#{line}",
-    sitename="AlternateVectors.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://rcalxrc08.github.io/AlternateVectors.jl",
-        edit_link="master",
-        assets=String[],
+    modules = [AlternateVectors],
+    authors = "Nicola <nicola.scaramuzzino@yandex.com> and contributors",
+    repo = "https://github.com/rcalxrc08/AlternateVectors.jl/blob/{commit}{path}#{line}",
+    sitename = "AlternateVectors.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://rcalxrc08.github.io/AlternateVectors.jl",
+        edit_link = "master",
+        assets = String[]
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"]
 )
 
-deploydocs(;
-    repo="github.com/rcalxrc08/AlternateVectors.jl",
-    devbranch="master",
-)
+deploydocs(; repo = "github.com/rcalxrc08/AlternateVectors.jl", devbranch = "master")
